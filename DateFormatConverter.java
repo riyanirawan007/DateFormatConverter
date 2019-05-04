9b47aa12e4c42c50755b683df54debe59eb03459
Riyan Saputra Irawan <riyansaputrai007@outlook.com>
2019-05-04 22:05:05 +0700

    Create DateFormatConverter.java

---
+++ DateFormatConverter.java
@@ -0,0 +1,63 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateFormatConverter
+{
+    private SimpleDateFormat simpleDateFormat;
+    private String finalPattern, strDateToConvert;
+    private Date dateToConvert;
+    private boolean isValidPattern;
+    private Locale locale;
+    public static final String PATTERN_DATE_SQL = "yyyy-MM-dd";
+    public static final String PATTERN_DATE_SPELL_COMMON = "EEEE, dd MMMM yyyy";
+
+    public DateFormatConverter withDate(String strDateToConvert)
+    {
+        this.strDateToConvert = strDateToConvert;
+        return this;
+    }
+
+    public DateFormatConverter withPatternConvert(String initPattern,String finalPattern,Locale locale)
+    {
+        this.finalPattern = finalPattern;
+        this.locale = locale;
+
+        simpleDateFormat = new SimpleDateFormat(initPattern, locale);
+        try
+        {
+            dateToConvert = simpleDateFormat.parse(strDateToConvert);
+            isValidPattern = true;
+        } catch (ParseException e)
+        {
+            isValidPattern = false;
+        }
+        return this;
+    }
+
+    public String doConvert()
+    {
+        if (isValidPattern)
+        {
+            simpleDateFormat = new SimpleDateFormat(finalPattern, locale);
+            return simpleDateFormat.format(dateToConvert);
+        } else
+        {
+            return strDateToConvert;
+        }
+    }
+
+    public static Date setZeroTime(Date date)
+    {
+        Calendar calendar=Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MILLISECOND,0);
+        date=calendar.getTime();
+        return date;
+    }
+}
